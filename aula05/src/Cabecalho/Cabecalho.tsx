b7d4380fb1738a5600bbcef9a0222252733adc7a
nyxtais <nyxtais@gmail.com>
2024-08-20 10:28:20 -0300

    FINALMENTE

---
+++ aula05/src/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,25 @@
+import { Cabecalho1Props,Cabecalho2Props } from "./../types";
+
+function Cabecalho({paginaProps,nrPaginaProps,statusProps,avisoProps,children}:Cabecalho1Props & Cabecalho2Props) {
+
+    // if(typeof nrPaginaProps === "number"){
+    //     document.title = statusProps+" - "+nrPaginaProps;
+    // }else{
+    //     nrPaginaProps = "..??..";
+    //     document.title = statusProps+" - "+nrPaginaProps;
+    // }
+
+    document.title = statusProps+" - "+nrPaginaProps;
+   
+    return(
+        <header>
+            <h1>{paginaProps+"\n- "+nrPaginaProps}</h1>
+            <button onClick={()=> avisoProps()}>Aviso</button>
+            <div>
+                {children}
+            </div>
+        </header>
+    );
+}
+
+export default Cabecalho;
