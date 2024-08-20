8365b477170ef94b9dfa73a6974929ff42a012c5
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-20 08:46:52 -0300

    Passando function como props.

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,10 +1,13 @@
-function Cabecalho(props:{tituloProps:string}) {
+function Cabecalho(props:{tituloProps:string,avisoProps:Function}) {
 
     document.title = props.tituloProps;
+
+    document.addEventListener("click",()=>{})
 
     return(
         <header>
             <h1>{props.tituloProps}</h1>
+            <button onClick={()=> props.avisoProps()}>Aviso</button>
         </header>
     );
 }
