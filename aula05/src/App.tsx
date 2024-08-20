97c8f41e86631a3ba72b3ef8dc0afc895c6ef659
LuizHNR <luizhneri12@gmail.com>
2024-08-20 08:51:47 -0300

    props

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -3,13 +3,16 @@ import Cabecalho from "./components/Cabecalho/Cabecalho";
 
 function App(){
 
-    const titulo: string = "Página inicial";
+    const titulo: string = "Página inicial...";
+
+    
+    const aviso = ()=> alert(titulo);
 
   return(
     <div>
       <h1>Componete App</h1>
       {/* Area de props */}
-      <Cabecalho tituloProps = {titulo} />
+      <Cabecalho tituloProps = {titulo} avisoProps = {aviso} />
     </div>
   )
 }
--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,19 @@
-function Cabecalho(props: {tituloProps:string}) {
+type CabecalhoProps = {
+    
+}
 
-    document.title = props.tituloProps;
+function Cabecalho({tituloProps,avisoProps}:{tituloProps:string,avisoProps:Function}) {
+
+    document.title = tituloProps;
+
+    document.addEventListener("click",()=>{})
 
     return(
         <header>
-            <h1>Cabecalho</h1>
+            <h1>{tituloProps}</h1>
+            <button onClick={()=> avisoProps()}>Aviso</button>
         </header>
-    )
+    );
 }
 
-export default Cabecalho
+export default Cabecalho;
