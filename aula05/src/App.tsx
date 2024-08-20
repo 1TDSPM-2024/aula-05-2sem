9578790dc559913ad237c9deac9b2ad460f723b4
VitorIsacB <vitorisac14@gmail.com>
2024-08-20 09:15:34 -0300

    Passando props como variável.

---
+++ aula05/src/App.tsx
@@ -0,0 +1,15 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
+function App(){
+
+    const titulo:string = "Página ...";
+
+  return(
+    <div>
+      <h1>Componente App</h1>
+      <Cabecalho tituloProps={titulo} />
+    </div>
+  );
+}
+
+export default App
